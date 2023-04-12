aa4446aa7e1c7bdba4eb00c0b131b40060048822
Beginner79 <bortolini.leonardo@gmail.com>
2023-04-12 20:37:29 -0300

    git push origin/main

---
+++ login/login.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    const $select = $("#email");
+
+    const limpiar = () => {
+        $select.empty();
+      };
+$("#btnLimpiar").click(limpiar);
+const Enviar = () => {
+    $select.submit();
+  };
+$("#btnEnviar").click(Enviar);
+alert("Bienvenido Usuario")
+  });
