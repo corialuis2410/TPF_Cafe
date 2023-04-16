fbfefec1976a59c6fdaec630f8e7a50369126761
Beginner79 <bortolini.leonardo@gmail.com>
2023-04-16 08:25:46 -0300

    login

--- login/login.js
+++ login/login.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
     const $select = $("#email");
 
-    const limpiar = () => {
-        $select.empty();
-      };
 $("#btnLimpiar").click(limpiar);
-const Enviar = () => {
-    $select.submit();
-  };
-/* $("#btnEnviar").click(Enviar);
-alert("Bienvenido Usuario")
-  });*/
+    const limpiar = () => {$select.empty();
+};
+
+$("#btnEnviar").click(enviar);
+    const enviar = () => {$select.submit();
+};
+})
+
